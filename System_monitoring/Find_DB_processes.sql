37247622c213f5eb602f405c744dbc55b159e46d
George Gu <ggu@imo-online.com>
2016-11-16 11:33:36 -0600

    add date difference calculation

--- System_monitoring/Find_DB_processes.sql
+++ System_monitoring/Find_DB_processes.sql
@@ -8,12 +8,12 @@ SELECT s.inst_id,
        p.spid,
        s.username,
        s.program,
-       s.SQL_EXEC_START
+       s.SQL_EXEC_START,
+       SYSDATE,
+       (SYSDATE-s.SQL_EXEC_START)*24*60 as ELAPSED_MINUTES        
 FROM   gv$session s
        JOIN gv$process p ON p.addr = s.paddr AND p.inst_id = s.inst_id
 WHERE  s.type != 'BACKGROUND'
-order by SQL_EXEC_START desc
+order by SQL_EXEC_START 
 ;
-
-
  
