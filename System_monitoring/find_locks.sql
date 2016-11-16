27f1f8e44786c7ee86f0d2c8a71be44cdf421d5c
George Gu <ggu@imo-online.com>
2016-11-16 17:10:38 -0600

    find active sql and locks

---
+++ System_monitoring/find_locks.sql
@@ -0,0 +1,38 @@
+-- this will find locked tables
+-- http://stackoverflow.com/questions/622289/how-to-check-oracle-database-for-long-running-queries 
+
+select
+  object_name, 
+  object_type, 
+  session_id, 
+  type,         -- Type or system/user lock
+  lmode,        -- lock mode in which session holds lock
+  request, 
+  block, 
+  ctime         -- Time since current mode was granted
+from
+  v$locked_object, all_objects, v$lock
+where
+  v$locked_object.object_id = all_objects.object_id AND
+  v$lock.id1 = all_objects.object_id AND
+  v$lock.sid = v$locked_object.session_id
+order by
+  session_id, ctime desc, object_name; 
+
+-- finding long operations (e.g. full table scans)
+
+SELECT sid, to_char(start_time,'hh24:mi:ss') stime, 
+message,( sofar/totalwork)* 100 percent 
+FROM v$session_longops
+WHERE sofar/totalwork < 1
+;
+
+-- This one shows SQL that is currently "ACTIVE"
+select S.USERNAME, s.sid, s.osuser, t.sql_id, sql_text
+from v$sqltext_with_newlines t,V$SESSION s
+where t.address =s.sql_address
+and t.hash_value = s.sql_hash_value
+and s.status = 'ACTIVE'
+and s.username <> 'SYSTEM'
+order by s.sid,t.piece
+; 
